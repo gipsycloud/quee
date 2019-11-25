38e520bdd106aa187456f1f9176e8ca304d9f616
aungwin <aungthiwin@sys-city.com>
2019-11-26 01:09:30 +0630

    adding quasar for ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="text" label="Standard" />
+
+      <q-input filled v-model="text" label="Filled" />
+
+      <q-input outlined v-model="text" label="Outlined" />
+
+      <q-input standout v-model="text" label="Standout" />
+
+      <q-input standout="bg-teal text-white" v-model="text" label="Custom standout" />
+
+      <q-input borderless v-model="text" label="Borderless" />
+
+      <q-input rounded filled v-model="text" label="Rounded filled" />
+
+      <q-input rounded outlined v-model="text" label="Rounded outlined" />
+
+      <q-input rounded standout v-model="text" label="Rounded standout" />
+
+      <q-input square filled v-model="text" label="Square filled" />
+
+      <q-input square outlined v-model="text" label="Square outlined" />
+
+      <q-input square standout v-model="text" label="Square standout" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    // HelloWorld
+  data () {
+    return {
+      text: ''
+    }
   }
 }
 </script>
