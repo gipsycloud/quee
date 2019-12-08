f969f00f7291d8bb7afd456adea68507ece1a526
aungwin <aungthiwin@sys-city.com>
2019-12-09 00:06:11 +0630

    modified src/router/index.js and added paths.js file in router file
    https://github.com/AshrafAlam/vue.pwa.weather.app/blob/pwa-support/src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,54 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Setting from '../views/Setting.vue'
-import Profile from '../views/Profile.vue'
-import Pageone from '../views/Pageone.vue'
-import Pagetwo from '../views/Pagetwo.vue'
-import Pagethree from '../views/Pagethree.vue'
+// import Home from '../views/Home.vue'
+// import Setting from '../views/Setting.vue'
+// import Profile from '../views/Profile.vue'
+// import Pageone from '../views/Pageone.vue'
+// import Pagetwo from '../views/Pagetwo.vue'
+// import Pagethree from '../views/Pagethree.vue'
+
+// Routes
+import paths from './paths'
+
+function route (path, view, name) {
+  return {
+    name: name || view,
+    path,
+    component: (resovle) => import(
+      `@/views/${view}.vue`
+    ).then(resovle)
+  }
+}
 
 
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/profile', name: 'profile', component: Profile },
-  { path: '/setting', name: 'setting', component: Setting },
-  { path: '/page1', name: 'pageone', component: Pageone },
-  { path: '/page2', name: 'pagetwo', component: Pagetwo },
-  { path: '/page3', name: 'pagethree', component: Pagethree },
+// Create a new router
+const router = new VueRouter({
+  mode: 'history',
+  routes: paths.map(path => route(path.path, path.view, path.name)).concat([
+    { path: '*', redirect: '/' }
+  ]),
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { selector: to.hash }
+    }
+    return { x: 0, y: 0 }
+  }
+})
+
+// vue.use(Meta)
+
+// const routes = [
+//   { path: '/', name: 'home', component: Home },
+//   { path: '/profile', name: 'profile', component: Profile },
+//   { path: '/setting', name: 'setting', component: Setting },
+//   { path: '/page1', name: 'pageone', component: Pageone },
+//   { path: '/page2', name: 'pagetwo', component: Pagetwo },
+//   { path: '/page3', name: 'pagethree', component: Pagethree },
   // {
   //   path: '/about',
   //   name: 'about',
@@ -24,13 +56,13 @@ const routes = [
   //   // this generates a separate chunk (about.[hash].js) for this route
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
+//   // }
+// ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
 
 export default router
