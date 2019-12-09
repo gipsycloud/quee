ca8f03ff407333c87beb2aed67e3db5a6a80b22e
aungwin <aungthiwin@sys-city.com>
2019-12-10 01:40:39 +0630

    modified profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,39 @@
 <template lang="html">
-	<div class="q-pa-lg">
+	<!-- <div class="q-pa-lg"> -->
+	<div class="">
+		<div id="profile">
+			<div class="column items-center">
+	      <img src="@/statics/img/avatar.svg" class="avatar">
+	    </div>
+	    <hr>
+	    <!-- <div class="column items-center"> -->
+	<!--     </div> -->
+      <!-- <div id="user-actions"> -->
+        <!-- <q-btn
+          size="7px"
+          color="primary"
+          label="Text"
+        />
+        <q-btn
+          size="7px"
+          color="primary"
+          label="Text"
+        />
+        <q-btn
+          size="7px"
+          color="primary"
+          label="Exit"
+          @click='logout'
+        /> -->
+        <!-- <button class="bordered blue small" ><i>person</i></button>
+        <button class="bordered blue small" ><i>lock</i></button>
+        <button class="bordered blue small" @click='logOut'><i>exit_to_app</i></button> -->
+     <!-- </div> -->
+    </div>
+    <div id="user-name">
+	  	<span class="text-white"> Aung Aung </span>
+	  </div>
+
 		<div class="q-gutter-xl" style="">
 			<q-input v-model="text" label="Name" required minlength="4" maxlength="8" size="10" />
 		</div>
@@ -28,5 +62,36 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style scoped>
+  .fixed-bottom {
+    margin-bottom: 1%;
+  }
+
+  .fixed-bottom a img {
+    width: 25px;
+    height: 25px;
+  }
+  .avatar{
+    /* padding: 20px; */
+    margin: 17px;
+  }
+  #profile {
+    height: 130px;
+    background-color: #009688;
+  }
+  #user-name {
+    left: 90px;
+    bottom: 77px;
+    position: relative;
+    width: 159px;
+  }
+  #user-actions {
+    left: 90px;
+    bottom: 71px;
+    position: relative;
+    width: 171px;
+  }
+  #menu-collapse {
+    margin-top: 5%;
+  }
 </style>
