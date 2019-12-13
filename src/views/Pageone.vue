12d5a9f7cd15c72dfbdea32faa8e776a0d5b616d
aungwin <aungthiwin@sys-city.com>
2019-12-13 17:24:33 +0630

    testing for user login page

--- src/views/Pageone.vue
+++ src/views/Pageone.vue
@@ -1,104 +1,99 @@
-<template>
-  <div class="container">
-    <div class="squares">
-      <div class="square" v-for="i in 4" :key="i"></div>
-    </div>
+<!-- <template>
+	<div class="q-pr-xr row items-start q-gutter-xr">
+    <q-card class="">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+      </q-card-section>
+
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+ -->
+ <template>
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Squares',
-};
+  data () {
+    return {
+      name: null,
+      age: null,
+
+      accept: false
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'You need to accept the license and terms first'
+        })
+      }
+      else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Submitted'
+        })
+      }
+    },
+
+    onReset () {
+      this.name = null
+      this.age = null
+      this.accept = false
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-$squares-size: 40px;
-$square-size: $squares-size / 2;
-$animation: 1s linear infinite;
-
-.container {
-  text-align: center;
-}
-
-.squares {
-  position: relative;
-  display: inline-block;
-  width: $squares-size;
-  height: $squares-size;
-  overflow: hidden;
-  transform-origin: bottom left;
-  animation: squares $animation;
-}
-
-.square {
-  position: absolute;
-  width: $square-size;
-  height: 20px;
-  background-color: white;
-  left: 0px;
-  top: 0px;
-
-  &:first-child {
-    top: $square-size;
-  }
-  &:nth-child(2) {
-    left: $square-size;
-    top: $square-size;
-    animation: square2 $animation;
-  }
-  &:nth-child(3) {
-    animation: square3 $animation;
-  }
-  &:last-child {
-    left: $square-size;
-    animation: square4 $animation;
-  }
-}
-
-@keyframes squares {
-  from {
-    transform: scale(1);
-  }
-  90% {
-    transform: scale(1);
-  }
-  to {
-    transform: scale(0.5);
-  }
-}
-@keyframes square2 {
-  from {
-    transform: translateY(-50px);
-  }
-  25% {
-    transform: translate(0);
-  }
-  to {
-    transform: translate(0);
-  }
-}
-@keyframes square3 {
-  from {
-    transform: translateY(-50px);
-  }
-  50% {
-    transform: translate(0);
-  }
-  to {
-    transform: translate(0);
-  }
-}
-@keyframes square4 {
-  from {
-    transform: translateY(-50px);
-  }
-  75% {
-    transform: translate(0);
-  }
-  to {
-    transform: translate(0);
-  }
-}
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
 </style>
