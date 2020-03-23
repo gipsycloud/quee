e6e8e4cb3a4262ffe097d31449b29edc2e155319
aungwin <aungwinygn1990@gmail.com>
2020-03-24 00:59:22 +0630

    adding shop-design in Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,195 @@
 <template>
-  <q-page class="bg-light-blue window-height window-width row justify-center items-center">
-    <q-card inline style="width: 500px">
-      <q-card-media>
-        <!-- <img src="~assets/donuts.png"> -->
-        <img src="@/assets/donuts.png" class="">
-      </q-card-media>
-      <q-card-title>
-        Cafe Basilico
-        <q-rating slot="subtitle" v-model="stars" :max="5" />
-        <div slot="right" class="row items-center">
-          <q-icon name="place" /> 250 ft
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Cafe Basilico
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
         </div>
-      </q-card-title>
-      <q-card-main>
-        <p>$・Italian, Cafe</p>
-        <p class="text-faded">Small plates, salads & sandwiches in an intimate setting.</p>
-      </q-card-main>
-      <q-card-separator />
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <!-- <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div> -->
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
       <q-card-actions>
-        <q-btn flat round small><q-icon name="event" /></q-btn>
-        <q-btn flat>5:30PM</q-btn>
-        <q-btn flat>7:30PM</q-btn>
-        <q-btn flat>9:00PM</q-btn>
-        <q-btn flat color="primary">Reserve</q-btn>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
       </q-card-actions>
     </q-card>
-  </q-page>
+
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Cafe Basilico
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <!-- <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div> -->
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Cafe Basilico
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Cafe Basilico
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login',
   data () {
     return {
-      email: '',
-      password: ''
+      stars: 4
     }
   }
 }
 </script>
 
-<style>
-.q-card {
-  width: 360px;
-}
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 100%
 </style>
